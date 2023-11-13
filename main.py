6323411973f90f535d87b7966314bbf6930ed701
EricFergs <135307549+EricFergs@users.noreply.github.com>
2023-11-12 19:17:26 -0800

    added count

--- main.py
+++ main.py
@@ -119,7 +119,8 @@ def student_class():
             for s in student_list:
                 classes = s.courses
                 for c in classes:
-                    class_info = {"id": c.id, "course_name": c.course_name, "time": c.time, "capacity": c.capacity}
+                    course_count = len(c.students)
+                    class_info = {"id": c.id, "course_name": c.course_name, "time": c.time, "capacity": c.capacity, "amount":course_count}
                     all_classes.append(class_info)
             return jsonify({"classes": all_classes})
         else:
